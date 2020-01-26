612d5b4079a91f867e747f1ce9688e28f7b6a41c
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2020-01-26 12:20:38 +0000

    10. Components, Props & PropTypes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
-  foo = () => 'Bars';
-
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
-
     return (
       <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </div>
     );
   }
