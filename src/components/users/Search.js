5ac758aa0813f174da3ae2d207510542ed19751b
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2020-02-01 16:24:16 +0000

    30. Create Reducer & Actions

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
+const Search = ({ setAlert, showClear, clearUsers }) => {
+  const githubContext = useContext(GithubContext);
   const [text, setText] = useState('');
 
   const onSubmit = e => {
@@ -9,7 +11,7 @@ const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
     if (text === '') {
       setAlert('Please enter something', 'light');
     } else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText('');
     }
   };
@@ -42,7 +44,6 @@ const Search = ({ searchUsers, setAlert, showClear, clearUsers }) => {
 };
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
